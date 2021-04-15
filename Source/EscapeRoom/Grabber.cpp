ba4a40e564d08247ca1e2b1d72fff0d21ae83a5e
Het Patel <47503085+HettyPatel@users.noreply.github.com>
2021-04-15 13:35:22 -0700

    Raycasting only when player clicks to grab the object.

--- Source/EscapeRoom/Grabber.cpp
+++ Source/EscapeRoom/Grabber.cpp
@@ -12,8 +12,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -21,18 +19,62 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Reporting for Duty"));
-
-	// ...
+	//Check for physics handle component
+	FindPhysicsHandle();
+	SetupInputComponent();
 	
 }
 
+void UGrabber::SetupInputComponent(){
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if(InputComponent){
+		UE_LOG(LogTemp, Warning, TEXT("Input Component Detected! %s"), *GetOwner()->GetName());
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this,&UGrabber::Release);
+	}
+
+}
+
+void UGrabber::FindPhysicsHandle(){
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle){
+		UE_LOG(LogTemp, Warning, TEXT("Physics Handle is found!"));
+	}
+	else{
+		UE_LOG(LogTemp, Error, TEXT("No Physics Handle Component is Found on %s"), *GetOwner()->GetName());
+	}
+}
+
+void UGrabber::Grab(){
+	UE_LOG(LogTemp, Warning, TEXT("Key Pressed!"));
+
+	// Only ray cast if button is pressed and see if we reach.
+	GetFirstPhysicsBodyInReach();
+
+	//Try and reach any actors with collosion channel set. 
+	
+	//if we hit something than attach the physics handle
+
+	//TODO attach a physics handle
+}
+
+void UGrabber::Release(){
+	UE_LOG(LogTemp, Warning, TEXT("Key Released!"));
+	//Releaase the physics handle.
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
+
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	//If Physics Handle is attached move the object we are holding.
+	
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const{
+	
 	//Get Player's view point 
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
@@ -44,13 +86,20 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	// Draw a line from player showing the reach
 
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(0,255,0), false, 0.f, 0, 5.f);
 
 	//Reachout from player! (Raycasting) out to a certain distance! (can change where they can reach)
-
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParams);
 	//See what the raycast is hitting! 
 
+	AActor* ActorHit = Hit.GetActor();
 
+	if(ActorHit){
+		UE_LOG(LogTemp, Warning, TEXT("Hit %s"), *ActorHit->GetName());
+	}
+
+	return Hit;
 
 }
 
